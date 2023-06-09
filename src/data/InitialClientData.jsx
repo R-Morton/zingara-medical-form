36dc7e65848d7f8ef933d1f14a1f89dacee121f1
R-Morton <remann12@gmail.com>
2023-06-09 15:34:55 +1000

    separated reducer and initial data into new files

---
+++ src/data/InitialClientData.jsx
@@ -0,0 +1,14 @@
+export const initialMedicalData = [
+    {
+        id: 1,
+        name: "",
+        email: "",
+        age: "",
+        gender: "",
+        notes: [{
+            id: 1,
+            dateCreatedAt: "",
+            content: ""
+        }]
+    }
+]
