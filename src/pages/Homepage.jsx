706f92f462f2bd644841a57b2a7ec9fb6334ae90
R-Morton <remann12@gmail.com>
2023-06-10 16:07:33 +1000

    added temp link from homepage to form

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
     return(
+        <div>
         <h1>Some homepage</h1>
+        <Link to={"/client/form"}>Interactive Form</Link>
+        </div>
     )
 }
